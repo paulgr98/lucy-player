c71907e97d40e99706da9e2c064cffda206f96d8
paulgr98 <pawelwnuk98@gmail.com>
2024-12-23 22:55:26 +0100

    Fix DB connection

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,24 +2,35 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config({ path: process.env.NODE_ENV === 'production' ? '.env' : './config.env' });
 
 const connectionString = process.env.CONNECTION_URI;
-const client = new MongoClient(connectionString);
+const client = new MongoClient(connectionString, {
+  ssl: true,
+  tls: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: {
+      version: '1',
+      strict: true,
+      deprecationErrors: true,
+  }
+});
 
 let dbConnection;
 
 module.exports = {
   connectToServer: async function () {
-    try {
-      await client.connect();
-      dbConnection = client.db("lucy");
-      console.log("Successfully connected to MongoDB.");
-      return dbConnection;
-    } catch (err) {
-      console.error("Error connecting to MongoDB:", err);
-      throw err;
-    }
+      try {
+          console.log("Attempting to connect to MongoDB...");
+          await client.connect();
+          dbConnection = client.db(process.env.DB_NAME || "your_database_name");
+          console.log("Successfully connected to MongoDB.");
+          return dbConnection;
+      } catch (err) {
+          console.error("Error connecting to MongoDB:", err);
+          throw err;
+      }
   },
 
   getDb: function () {
-    return dbConnection;
+      return dbConnection;
   },
 };
